9d9155a7ecbd1a08d775999a05c6fc8b115d75f7
imp <patrick.imboden@hslu.ch>
2021-08-30 18:13:16 +0200

    feat: adding some SEO things

---
+++ ui/components/cbSpinner.vue
@@ -0,0 +1,14 @@
+<template>
+<v-row style="text-align:center">
+          <v-col cols="12">
+  <div class="lds-heart"><div></div></div>
+          </v-col>
+</v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
